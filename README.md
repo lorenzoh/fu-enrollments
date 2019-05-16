31caa9010ab9bde69dee695461a84f695716c7b7
lorenzoh <lorenz.ohly@gmail.com>
2019-05-16 15:47:20 +0200

    Added script for scraping links to pdfs

---
+++ README.md
@@ -0,0 +1,7 @@
+# FU enrollments
+
+## Steps
+
+## 1. Scrape list of `.pdf`s from website
+## 2. Download `.pdf`s
+## 3. Extract data from `.pdf`s to `.csv`
--- scripts/scrape_pdf_list.py
+++ scripts/scrape_pdf_list.py
@@ -1,5 +1,5 @@
 """
-Scrapes the links to all PDF files from the FU website and downloads the PDFs
+Scrapes the links to all PDF files from the FU website
 """
 from pathlib import Path
 import requests
@@ -8,34 +8,44 @@ import pandas as pd
 
 URL = "https://www.fu-berlin.de"
 LIST_PATH = "/studium/studienorganisation/immatrikulation/weitere-angebote/statistik/index.html"
+OUT_PATH = Path("data/links.csv")
 
 
 def main():
     list_soup = get_soup(URL + LIST_PATH)
     semester_paths = [el.attrs["href"] for el in list_soup.select("h3 a")]
 
+    pdfs = []
+
     for semester_url in [URL + path for path in semester_paths]:
-        scrape_pdfs_semester(semester_url)
+        pdfs.extend(scrape_pdfs_semester(semester_url))
+
+    df = pd.DataFrame(pdfs)
+
+    df.to_csv(OUT_PATH, index=None)
 
 
 def get_soup(url):
     r = requests.get(url)
     assert r.status_code == 200
-    return BeautifulSoup(r.text)
+    return BeautifulSoup(r.text, features="html.parser")
 
 
 def scrape_pdfs_semester(semester_url):
-    
+
     soup = get_soup(semester_url)
-    semester = " ".join(soup.select("h1").split()[-2:])  # Parse semester from title
+    semester = " ".join(soup.select_one("h1:nth-child(1)").text.split()
+                        [-2:])  # Parse semester from title
     pdf_links = soup.select(".editor-content.hyphens a")
+
+    pdfs = []
     for link in pdf_links:
-        url = URL + link.attrs["href"]
-        kind = link.text
-        tags = link.attrs[""]
+        pdf = {"semester": semester}
+        pdf["url"] = URL + link.attrs["href"]
+        pdf["kind"] = link.text
+        pdfs.append(pdf)
 
-
-    print(pdf_links)
+    return pdfs
 
 
 if __name__ == "__main__":
