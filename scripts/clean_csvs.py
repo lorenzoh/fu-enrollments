0c00ee2a85d5ac06e742d0f016ae4cb3cfb8e85e
lorenzoh <lorenz.ohly@gmail.com>
2019-06-16 13:16:41 +0200

    Put conversion script under dvc

---
+++ scripts/clean_csvs.py
@@ -0,0 +1,16 @@
+"""
+
+After conversion from pdf to csv, there are still some parsing errors to be resolved:
+
+1. empty rows
+
+
+2. entries with long subject names span over multiple rows
+
+3. empty columns
+
+Detected with pandas, columns are deleted.
+
+
+
+"""
